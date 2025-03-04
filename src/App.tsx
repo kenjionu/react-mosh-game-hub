549139c6b13f9a604b2363391fcfe3ea4487c645
Cesar Casares <cacjmuse@gmail.com>
2025-03-03 21:05:31 -0500

    feat: filtering games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import GenreList from "./components/GenreList/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedPlatform, setPlatform] = useState<Platform | null>(null);
+  
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
@@ -27,8 +30,8 @@ function App() {
         </GridItem>
         <GridItem area='main'>
           <Box paddingLeft={2}>
-          <PlatformSelector />
-          <GameGrid selectedGenre={selectedGenre}/>
+          <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setPlatform(platform)}/>
+          <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
           </Box>
         </GridItem>
 
--- src/components/PlatformSelector/PlatformSelector.tsx
+++ src/components/PlatformSelector/PlatformSelector.tsx
@@ -1,8 +1,14 @@
+import { Platform } from '@/hooks/useGames';
 import usePlatforms from '../../hooks/usePlatforms';
 import { Box, Button, MenuContent, MenuItem, MenuItemCommand, MenuRoot, MenuTrigger } from '@chakra-ui/react'
 import { LuArrowRightFromLine, LuChevronsRight, LuScissors } from 'react-icons/lu'
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
     const { data, error } = usePlatforms();
 
     if (error) return null;
@@ -10,12 +16,14 @@ const PlatformSelector = () => {
         <MenuRoot>
             <MenuTrigger asChild>
                 <Button variant="outline" size="sm">
-                    Platforms <LuChevronsRight />
+                    {selectedPlatform?.name || 'Platforms'} <LuChevronsRight />
                 </Button>
             </MenuTrigger>
             <MenuContent>
                 {data.map(platform => (
-                    <MenuItem key={platform.id} value="cut" valueText="cut">
+                    <MenuItem
+                    onClick={() => onSelectPlatform(platform)}
+                    key={platform.id} value="cut" valueText="cut">
 
                         <Box flex="1">{platform.name}</Box>
                     </MenuItem>
